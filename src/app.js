a1f21b8efade97bc91254736e6f00a5dc45b2eb5
pujeetas <pujeeta98@gmail.com>
2025-11-11 22:25:54 +0530

    First Commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("hello =");
+});
+
+app.listen(3000, () => {
+  console.log("Hello 3000");
+});
