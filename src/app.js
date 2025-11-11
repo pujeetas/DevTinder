6e9bedc8dcec6e79a6f70bf021df8db7d407a40a
pujeetas <pujeeta98@gmail.com>
2025-11-11 23:45:23 +0530

    tested APIs to learn

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("hello =");
+app.get("/test/:userId", (req, res) => {
+  const userId = req.params.userId;
+  if (userId === "1000") {
+    res.send(`Hello user ${userId}`);
+  } else res.send("User not found");
+});
+
+app.post("/test", (req, res) => {
+  res.send("This is a post call");
 });
 
 app.listen(3000, () => {
